5ef1e308d8c8974a4074c35a0fd2923b895d3fed
rudnicka1 <35965571+rudnicka1@users.noreply.github.com>
2018-03-27 10:15:06 -0400

    Updated Shuffler.java rudnicka1
    Unsure if I am doing this right for GitHub, testing to see... may have to delete after.

--- Shuffler.java
+++ Shuffler.java
@@ -7,7 +7,8 @@ package smartshuffle;
 
 /**
  *
- * @author laubera1
+ * @author rudnicka1
+ * @recent_version_date: 27/03/17
  */
 public class Shuffler {
     public void initialize() {
@@ -15,12 +16,53 @@ public class Shuffler {
         datab.populateSongs();
         UI ui = new UI();
         ui.initialize();
-        Playlist playlist = new Playlist();
-        CategoryHolder Categories = new CategoryHolder();
+        Playlist songQueue = new Playlist();
+        Playlist history = new Playlist();
+        Playlist unplayed = new Playlist();
+        Category main = new Category();
     }
     
-    public void Shuffle() {
-        
-        //do the shuffle
+    // Master function called for adding another song to shuffle queue
+    public void Shuffle() 
+    {        
+        // Pull current unplayed playlist
+
+        // Sort playlist by each category 
+
+            // For each category, sort (see Playlist.CategorySort)
+
+        // Add top value to song queue
+    }
+    //upvote
+    public void Upvote()
+    {
+        //grab song at top of songQueue
+        Song current = songQueue.top();
+
+        //for each tag in song.tags, check if it exists
+        for(Pair<String, Float> i : current.tags)
+        {
+            bool exist = false;
+            int index_of_i = null;
+            //if not there, add
+            for(Tag j : main.tags)
+            {
+                if((String)(j.getName()).compareTo(i.getName()))
+                {
+                    exist = true;
+                    index_of_i = main.tags.indexOf(j);
+                    break;
+                }
+            }
+            //if not, make it so
+            if(!exist)
+            {
+                main.tags.add(i)
+                index_of_i = tags.size() - 1;
+            }
+
+            //take song.tag.value and tick up
+            (Tag)(main.tags.get(index_of_i)).value += i.value;
+        }
     }
 }
